b8503369bb7a818a6789b0f5b526a60a9ded1bef
Per Orest <per.orest@outlook.com>
2019-01-28 13:49:09 +0100

    Add project files.

---
+++ MVCOne/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCOne.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
