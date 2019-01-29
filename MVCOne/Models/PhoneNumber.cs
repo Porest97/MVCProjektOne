08ca2f45ba499ab6e17632d714ea03a58e9cc7cf
Per Orest <per.orest@outlook.com>
2019-01-29 22:23:09 +0100

    3more classes

---
+++ MVCOne/Models/PhoneNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCOne.Models
+{
+    public class PhoneNumber
+    {
+        public int Id { get; set; }
+        public string Phonenumber { get; set; }
+        public int PersonId { get; set; }
+    }
+}
